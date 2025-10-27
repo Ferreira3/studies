2b5902cbe84936baeca9a0c1769dbd45284a01ab
José Eduardo Ferreira <54028051+Ferreira3@users.noreply.github.com>
2025-10-27 18:13:10 -0300

    Add exercise 115 with menu implementation and create interface module

--- Python3 - cursoemvideo.com/Mundo 3/Ex115a.py
+++ Python3 - cursoemvideo.com/Mundo 3/Ex115a.py
@@ -1,4 +1,18 @@
 #Exercício Python 115a:
 #Vamos criar um menu em Python, usando modularização.
 
-...
+from exer115.lib.interface import *
+from time import sleep
+
+while True:
+    resp = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resp == 1:
+        cabecalho("Opção 1")
+    elif resp == 2:
+        cabecalho("Opção 2")
+    elif resp == 3:
+        cabecalho("Saindo do sistema... Até logo!")
+        break
+    else:
+        print("ERRO! Digite uma opção válida!")
+    sleep(1)
